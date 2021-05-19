2a35ad161b232e6be96331bfc06bd0aaaabb1689
Mr1Mao <1195419991@qq.com>
2021-05-19 14:24:10 +0800

    补充

---
+++ nuxt.config.js
@@ -0,0 +1,87 @@
+const env = require('./env')
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'rooftop-blog',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    'element-ui/lib/theme-chalk/index.css',
+    { src: '~/assets/css/main.scss', lang: 'scss' },
+    // { src: '~/assets/css/animation.scss', lang: 'scss' },
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+    '@/plugins/element-ui',
+    { src: '@/plugins/aos', ssr: false, mode: 'client' },
+    { src: '@/plugins/api' },
+    { src: '@/plugins/vue-mavon-editor', ssr: false },
+    '@/plugins/global',
+    { src: '@/plugins/localStorage', ssr: false }
+    // '@/plugins/axios'
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    // '@nuxtjs/auth-next',
+    '@nuxtjs/style-resources',
+    '@nuxtjs/proxy'
+  ],
+  proxy: {
+    // see Proxy section
+    '/api': {
+      target: env[process.env.MODE].ENV_API,
+      // ws: true,
+      changeOrigin: true,
+      pathRewrite:{
+        "^/api":"",
+    }
+    }
+  },
+  styleResources: {
+    scss: '~assets/css/main.scss'
+  },
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    proxy: true,
+    prefix: '/api',
+    baseURL: env[process.env.MODE].ENV_API
+    // baseURL: 'https://mock.yonyoucloud.com/mock/17387'
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+    transpile: [/^element-ui/],
+
+  },
+  router: {
+    middleware: [
+      // 'authenticated'
+      // 'auth',
+      //   'no-auth'
+    ]
+  }
+}
